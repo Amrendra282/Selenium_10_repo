3e9f2363028db863e290ea2f54c06189318a5aeb
Amrendra282 <amrendramishra54@gmail.com>
2025-02-01 10:38:03 +0530

    Update SampleTest.java

--- SampleMaven_10/src/test/java/Demo/SampleTest.java
+++ SampleMaven_10/src/test/java/Demo/SampleTest.java
@@ -7,6 +7,6 @@ public class SampleTest {
  @Test
  public void launch() {
 	 Reporter.log("Sample Test class get executed", true);
-	 Reporter.log("Sample Test class get executed", true);
+	 Reporter.log("Modified in GitHub", true);
  }
 }
