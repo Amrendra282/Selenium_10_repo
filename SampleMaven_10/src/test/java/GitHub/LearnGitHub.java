c0f48e949738ffdaee103db49d25f28a7d13e9aa
Amrendra282 <amrendramishra54@gmail.com>
2025-02-01 10:55:42 +0530

    New Package added

---
+++ SampleMaven_10/src/test/java/GitHub/LearnGitHub.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnGitHub {
+	@Test
+	public void learn() {
+		Reporter.log("GitHub class added", true);
+	}	
+}
